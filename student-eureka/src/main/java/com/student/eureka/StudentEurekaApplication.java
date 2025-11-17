b376207594648802a4477b780e72461fe528227e
Musa Daniel <jtccontractsservicesng@yahoo.com>
2025-11-17 14:05:41 +0100

    Additional packages added to the module. Student-info,

--- student-eureka/src/main/java/com/student/eureka/StudentEurekaApplication.java
+++ student-eureka/src/main/java/com/student/eureka/StudentEurekaApplication.java
@@ -2,9 +2,10 @@ package com.student.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
+@EnableEurekaServer
 public class StudentEurekaApplication {
 
 	public static void main(String[] args) {
